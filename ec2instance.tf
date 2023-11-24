27d0b8cde62a2ba9373b5d440d35882150573d5c
saiurakrishna <34538779+saiurakrishna@users.noreply.github.com>
2023-11-24 07:54:36 +0530

    Update ec2instance.tf

--- ec2instance.tf
+++ ec2instance.tf
@@ -1,18 +1,10 @@
-provider "aws" {
-  region = "us-east-1" # Change this to your desired AWS region
-}
+resource "aws_instance" "web" {
+  ami           = "ami-0f34c5ae932e6f0e4" # Replace with the desired AMI ID for your region
+  instance_type = "t2.micro"              # Replace with the desired instance type
 
-
-resource "aws_key_pair" "example" {
-  key_name = "example-key"
-  public_key = file("~/.ssh/id_rsa.pub") # Replace with the path to your public key
-}
-
-
-resource "aws_instance" "example" {
-  ami           = "ami-0230bd60aa48260c6"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.example.key_name
+  tags = {
+    Name = "MyEC2Instance" # Replace with your desired instance name
+  }
 
   user_data = <<-EOF
               #!/bin/bash
@@ -21,9 +13,39 @@ resource "aws_instance" "example" {
               service httpd start
               echo "<html><body><h1>Hello from Terraform EC2</h1></body></html>" > /var/www/html/index.html
               EOF
-
-  tags = {
-    Name = "example-instance"
-  }
 }
 
+resource "aws_instance" "web2" {
+  ami           = "ami-0f34c5ae932e6f0e4" # Specify the desired AMI
+  instance_type = "t2.micro"              # Specify the instance type
+  tags = {
+    Name = "web-server" # Replace with your desired instance name
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              service httpd start
+              echo "<html><body><h1>Hello from Terraform EC2 Instance 2</h1></body></html>" > /var/www/html/index.html
+              EOF
+
+}
+
+resource "aws_instance" "web3" {
+  ami           = "ami-0230bd60aa48260c6" # Replace with the desired AMI ID for your region
+  instance_type = "t2.micro"
+  key_name = "linux-server-new.pem"
+  vpc_security_group_ids = ["sg-0d0ac027dc2a538fa"]
+  tags = {
+    Name = "linux-instance-new" # Replace with your desired instance name
+  }
+
+}
+
+terraform {
+  backend "s3" {
+    bucket = "demo-terraform-s3-backend-srish-check" # Replace with your S3 bucket name
+    key    = "terraform.tfstate"
+    region = "us-east-1" # Replace with your desired AWS region
+  }
+}
